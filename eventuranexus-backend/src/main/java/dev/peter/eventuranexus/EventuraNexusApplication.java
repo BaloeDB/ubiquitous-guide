ecf13724cda25d4631c2e8535c8221accaa9ec21
Peter Diepstraten <psrdiepstraten@gmail.com>
2024-04-30 17:12:41 +0200

    Backend folder

---
+++ eventuranexus-backend/src/main/java/dev/peter/eventuranexus/EventuraNexusApplication.java
@@ -0,0 +1,13 @@
+package dev.peter.eventuranexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventuraNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventuraNexusApplication.class, args);
+	}
+
+}
---
+++ eventuranexus-backend/src/test/java/dev/peter/eventuranexus/EventuraNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.peter.eventuranexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventuraNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
